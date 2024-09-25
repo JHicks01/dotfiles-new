1a61e39de36110bcd85583c2a29f54bc65f71401
James Hicks <jmhicks001@gmail.com>
2024-09-25 20:14:55 +0100

    Fix indent/tab settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -36,59 +36,62 @@ vim.opt.cursorline = true
 vim.opt.scrolloff = 10
 vim.opt.clipboard = 'unnamedplus'
 
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
 -- Setup lazy.nvim
 require('lazy').setup({
-  {
-    'catppuccin/nvim',
-    priority = 1000,
-    init = function()
-      vim.cmd.colorscheme 'catppuccin'
-    end,
-  },
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = {
-      theme = 'catppuccin'
+    {
+        'catppuccin/nvim',
+        priority = 1000,
+        init = function()
+            vim.cmd.colorscheme 'catppuccin'
+        end,
     },
-    config = function()
-      require('lualine').setup {
-        options = {
-          theme = "catppuccin"
-        }
-      }
-    end
-  },
-  {
-    'neovim/nvim-lspconfig',
-    config = function()
-      require'lspconfig'.clangd.setup{}
-      require'lspconfig'.rust_analyzer.setup{}
-    end 
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        opts = {
+            theme = 'catppuccin'
+        },
+        config = function()
+            require('lualine').setup {
+                options = {
+                    theme = "catppuccin"
+                }
+            }
+        end
+    },
+    {
+        'neovim/nvim-lspconfig',
+        config = function()
+            require'lspconfig'.clangd.setup{}
+            require'lspconfig'.rust_analyzer.setup{}
+        end 
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function () 
+            local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-        ensure_installed = { "c", "lua", "cpp", "rust", "python"},
-        sync_install = false,
-        highlight = { enable = true },
-          indent = { enable = true }, -- TODO: This isn't working very well
-        })
-    end
-  },
-  {
-    "ibhagwan/fzf-lua",
-    -- optional for icon support
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      -- calling `setup` is optional for customization
-      require("fzf-lua").setup({})
-    end
-  }
+            configs.setup({
+                ensure_installed = { "c", "lua", "cpp", "rust", "python"},
+                sync_install = false,
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end
+    },
+    {
+        "ibhagwan/fzf-lua",
+        -- optional for icon support
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            -- calling `setup` is optional for customization
+            require("fzf-lua").setup({})
+        end
+    }
 })
 
 -- Keymap. TODO: Move to a different file
